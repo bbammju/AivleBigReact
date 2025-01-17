60e6bcd6a6892e4ba19d08d273d5c1a8f14966d2
bbammju <stwinsilver2@gmail.com>
2025-01-17 11:03:24 +0900

    결과리스트 1차 완료

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import InputModal from './pages/InputModal'; // InputModal 컴포넌트를 import
+import RsltList from './pages/RsltList';
+import GuModal from "./components/guModal";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* 기본 경로("/")에 InputModal 컴포넌트를 렌더링 */}
         <Route path="/" element={<InputModal />} />
+        <Route path="/list" element={<RsltList />} />
+        <Route path="/list1" element={<GuModal />} />
       </Routes>
     </BrowserRouter>
   );
