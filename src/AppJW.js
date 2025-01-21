6206667199b06174e37ad54dab6d54405eba5212
jiwon <ji_ji1997@naver.com>
2025-01-21 15:35:37 +0900

    withdraw, editprofile, mypage 구현, 파일이름 정리리

--- src/AppJW.js
+++ src/AppJW.js
@@ -4,9 +4,11 @@ import Main from './pages/Main'; // 메인 페이지
 import EmailCheck from './pages/EmailCheck'; // 이메일 중복 체크 페이지
 import Signup from './pages/Signup'; // 회원가입
 import RsltList from './pages/RsltList';
-import Mypage from './pages/Mypage'; // 마이 페이지
+import Mypage from './pages/MyPage'; // 마이 페이지
 import FavoritesPage from "./pages/FavoritesPage"; // 관심주택 페이지
 import PwChange from "./pages/PwChange";
+import EditProfile from "./pages/EditProfile";
+import WithdrawalPage from "./pages/WithdrawalPage";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -15,8 +17,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/mypage" element={<Mypage />} />
-        <Route path="/FavoritesPage" element={<FavoritesPage />} />
-        <Route path="/PwChange" element={<PwChange />} />
+        <Route path="/editprofile" element={<EditProfile />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="/pwchange" element={<PwChange />} />
+        <Route path="/withdrawal" element={<WithdrawalPage />} />
       </Routes>
     </BrowserRouter>
   );
