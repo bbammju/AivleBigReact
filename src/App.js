1f3e75f28f528e0df39c260e0ea79ed398798d26
jiwon <ji_ji1997@naver.com>
2025-01-21 16:08:19 +0900

    app 수정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import InputModal from './pages/InputModal'; // InputModal 컴포넌트를 import
 import Main from './pages/Main'; // 메인 페이지
-import Mypage from './pages/Mypage'; // 마이 페이지
+// import Mypage from './pages/MyPage'; // 마이 페이지
 import EmailCheck from './pages/EmailCheck'; // 이메일 중복 체크 페이지
 import Signup from './pages/Signup'; // 회원가입
 import RsltList from './pages/RsltList';
+// import Community from './pages/Community'; // 게시판
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -13,7 +14,8 @@ function App() {
       <Routes>
         {/*<Route path="/signup" element={<Signup />} /> */}
         {/* <Route path="/login" element={<Login />} /> */}
-        <Route path="/mypage" element={<Mypage />} />
+        {/* <Route path="/mypage" element={<Mypage />} /> */}
+        {/* <Route path="/community" element={<Community />} /> */}
         <Route path="/" element={<Main />} />
         <Route path="/input" element={<InputModal />} />
         <Route path="/email-check" element={<EmailCheck />} />
