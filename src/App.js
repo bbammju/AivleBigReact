d98ca38d992a4802dcfa6e1345d1d2a619d87b3c
sunchae <tjsco94@naver.com>
2025-01-24 15:56:52 +0900

    App.js 통합, 불필요 파일 삭제제

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import InputModal from './pages/InputModal'; // Input모달창창
 import Main from './pages/Main'; // 메인 페이지
 import RsltList from './pages/RsltList';
-import EmailCheck from './pages/EmailCheck'; // 이메일 중복 체크 페이지
+// import EmailCheck from './pages/EmailCheck'; // 이메일 중복 체크 페이지
 import Signup from './pages/Signup'; // 회원가입
 import ForgotPassword  from './pages/ForgotPassword'; // 비밀번호 찾기
-import Mypage from './pages/MyPage'; // 마이 페이지
+import Mypage from './pages/Mypage'; // 마이페이지
 import FavoritesPage from './pages/FavoritesPage'; // 관심주택 페이지
 import PwChange from './pages/PwChange';
 import EditProfile from "./pages/EditProfile";
@@ -16,14 +16,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        {/*<Route path="/signup" element={<Signup />} /> */}
-        {/* <Route path="/login" element={<Login />} /> */}
-        {/* <Route path="/mypage" element={<Mypage />} /> */}
-        {/* <Route path="/community" element={<Community />} /> */}
+      <Routes>        
         <Route path="/" element={<Main />} />
         <Route path="/input" element={<InputModal />} />
-        <Route path="/email-check" element={<EmailCheck />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/list" element={<RsltList />} />
         <Route path="/forgot-password" element={< ForgotPassword />} />
